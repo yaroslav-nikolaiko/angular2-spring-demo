bd00fb19bfa0640457d382c808be64e13c6eeb90
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2016-07-13 03:48:21 +0300

    working on select

--- src/main/webapp/app/src/post/posts.component.ts
+++ src/main/webapp/app/src/post/posts.component.ts
@@ -4,19 +4,24 @@ import {PostService} from './post.service';
 import {Post, Comment} from "./post";
 import {SpinnerComponent} from "../utils/spinner.component";
 import {Observable} from "rxjs/Rx";
+import {User} from "../user/user";
+import {UserService} from "../user/user.service";
 
 @Component({
     templateUrl: 'app/templates/posts.html',
-    providers: [PostService],
+    providers: [PostService, UserService],
     directives: [SpinnerComponent]
 })
 export class PostsComponent implements OnInit {
     posts: Post[] = [];
+    users: User[] = [];
     isLoading = true;
     currentPost;
+    selectedUser = "";
     comments: Observable<Comment[]>;
 
-    constructor(private _postService: PostService) {
+    constructor(private _postService: PostService,
+                private _userService: UserService) {
     }
 
     ngOnInit() {
@@ -26,10 +31,15 @@ export class PostsComponent implements OnInit {
                 null,
                 () => this.isLoading = false
             );
+        this._userService.getUsers().subscribe(u=>this.users = u);
     }
 
     select(post: Post){
         this.comments = post.comments();
         this.currentPost = post;
+    }
+
+    reloadPosts(event){
+        console.log(event);
     }
 }
