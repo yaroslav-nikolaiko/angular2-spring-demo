94a82a4143100cc88b72dfb88cb66d7efaf05de8
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2016-08-02 00:48:24 +0300

    REST service add found account by comment method

--- src/main/java/education/demo/dao/AccountRepository.java
+++ src/main/java/education/demo/dao/AccountRepository.java
@@ -1,7 +1,13 @@
 package education.demo.dao;
 
 import education.demo.model.Account;
+import education.demo.model.Comment;
+import education.demo.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +15,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long>{
+
+    @RestResource(path = "comment", rel = "byComment")
+    Account findByPostsComments(@Param("href") Comment comment);
 }
